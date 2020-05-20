f08001f97dfee87ae1a23a1a85a4be92a974c21d
Aaron Mcnulty <amcnulty@apttus.com>
2020-05-20 12:21:51 -0500

    Documentation Updates

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -16,7 +16,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">Apttus Ecommerce Spring &#x27;20</a>
+                    <a href="index.html" data-type="index-link">Apttus Digital Commerce Spring &#x27;20</a>
                 </li>
 
                 <li class="divider"></li>
@@ -59,7 +59,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                         <a href="additional-documentation/upgrade-partner-commerce-template-from-winter-v1903-to-spring-v2001.html" data-type="entity-link" data-context-id="additional">Upgrade partner commerce template from Winter v1903 to Spring v2001</a>
                                     </li>
                                     <li class="link ">
-                                        <a href="additional-documentation/upgrade-ecommerce-templat-from-summer-v1902-to-spring-v2001.html" data-type="entity-link" data-context-id="additional">Upgrade ecommerce templat from Summer v1902 to Spring v2001</a>
+                                        <a href="additional-documentation/upgrade-ecommerce-template-from-summer-v1902-to-spring-v2001.html" data-type="entity-link" data-context-id="additional">Upgrade ecommerce template from Summer v1902 to Spring v2001</a>
                                     </li>
                         </ul>
                     </li>
